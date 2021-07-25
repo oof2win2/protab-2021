e5526bb55af6de43da7753315ac66fa83710468c
oof2win2 <honza.koco44@gmail.com>
2021-07-25 21:46:14 +0200

    :sparkles: AR1

---
+++ archeologie/ar1.py
@@ -0,0 +1,24 @@
+from tqdm import tqdm
+
+with open("ar1.in", "r") as file:
+    data = [[y for y in x] for x in file.read().split("\n")]
+
+
+def findNeighbors(x: int, y: int):
+	data[y][x] = "."
+	for dx, dy in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+		if data[y+dy][x+dx] == " ":
+			data[y+dy][x+dx] = "."
+			findNeighbors(x+dx, y+dy)
+
+countAreas = 0
+for y, row in enumerate(data):
+	for x, space in enumerate(row):
+		if space == " ":
+			findNeighbors(x, y)
+			countAreas += 1
+# n = findNeighbors(2, 19)
+# print(n)
+
+# n = findNeighbors(2, 18)
+print(countAreas)
