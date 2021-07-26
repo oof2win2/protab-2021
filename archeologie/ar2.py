4ce8e1db811aa419431e43824244944e4f4b3a7b
oof2win2 <honza.koco44@gmail.com>
2021-07-26 17:24:25 +0200

    :construction: functional BFS!

--- archeologie/ar2.py
+++ archeologie/ar2.py
@@ -1,78 +1,99 @@
 import time
-with open("ar2.in", "r") as file:
-	data = [[y for y in x] for x in file.read().split("\n")]
+# with open("ar2.in", "r") as file:
+# 	data = [[y for y in x] for x in file.read().split("\n")]
 
+# class Vertex:
+# 	def __init__(this, x: int, y: int, dist: int):
+# 		this.x = x
+# 		this.y = y
+# 		this.dist = dist
+	# def findNeighbors(this):
+	# 	return findNeighbors(this)
+	# def get_visited(this):
+	# 	return isVisited(this)
+	# visited: bool = property(get_visited)
 
-outDat = data[::]
+# def findNeighbors(v: Vertex) -> list[Vertex]:
+	# neigh: list[Vertex] = []
+	# for dx, dy in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+	# 	if v.x+dx > len(data[0]) or v.x+dx < 0: continue
+	# 	if v.y+dy > len(data) or v.y+dy < 0: continue
+	# 	value = data[v.x+dx][v.y+dy]
+	# 	if value != " ": continue
+	# 	print(v.y, v.x)
+	# 	neigh.append(Vertex(v.x+dx, v.y+dy, v.dist+1))
+	# return neigh
 
-# def findNeighbors(x: int, y: int, furthest: int) -> int:
-# 	data[y][x] = f"{furthest}"
-# 	for dx, dy in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
-# 		if y+dy < 0 or y+dy > len(data): continue
-# 		if x+dx < 0 or x+dx > len(data[0]): continue
-# 		if data[y + dy][x + dx] == " ":
-# 			data[y + dy][x + dx] = f"{furthest+1}"
-# 			outDat[y+dy][x+dx] = furthest
-# 			dist = findNeighbors(x + dx, y + dy, furthest+1)
-# 			if dist > furthest: furthest = dist
-# 	return furthest
+# visited: list[Vertex] = []
 
+# def isVisited(toCheck: Vertex):
+# 	for vertex in visited:
+# 		if vertex.x == toCheck.x and vertex.y == vertex.y: return True
+# 	return False
 
-# print(findNeighbors(1, 1, 0), "furthest")
+# def bfs(start: Vertex):
+# 	global visited
+# 	visited.append(start)
+# 	queue = [start]
 
-# # output
+# 	while queue:
+# 		current = queue.pop(0)
+# 		neighbors = current.findNeighbors()
+# 		print(len(queue), len(visited), len(neighbors))
+# 		for neighbor in neighbors:
+# 			if not neighbor.visited:
+# 				visited.append(neighbor)
+# 				queue.append(neighbor)
+
+# bfs(Vertex(1, 1, 0))
+
+# longest = 0
+# for v in visited:
+# 	if v.dist > longest: longest = v.dist
+# print(longest)
+
+# for v in visited:
+# 	if v.visited:
+# 		data[v.y][v.x] = "."
+
 # output: list[str] = []
-# for row in outDat:
-# 	for i, space in enumerate(row):
-# 		row[i] = "{:3s}".format(space)
-# 	joined = " ".join(row)
-# 	output.append(joined)
+# for row in data:
+# 	output.append(" ".join(row))
 
 # with open("ar2.out", "w+") as file:
 # 	file.write("\n".join(output))
 
-class Vertex:
-	def __init__(this, x: int, y: int, dist: int):
-		this.x = x
-		this.y = y
-		this.dist = dist
-	def findNeighbors(this):
-		return findNeighbors(this)
 
-def findNeighbors(v: Vertex) -> list[Vertex]:
-	neigh: list[Vertex] = []
+with open("ar2.in", "r") as file:
+	data = [[y for y in x] for x in file.read().split("\n")]
+
+def getNeighbors(x: int, y: int) -> list[list[int]]:
+	neigh: list[list[int]] = []
 	for dx, dy in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-		if v.x+dx > len(data[0]) or v.x+dx < 0: continue
-		if v.y+dy > len(data) or v.y+dy < 0: continue
-		neigh.append(Vertex(v.x+dx, v.y+dy, v.dist+1))
+		if x+dx > len(data[0]) or x+dx < 0: continue
+		if y+dy > len(data) or y+dy < 0: continue
+		value = data[x+dx][y+dy]
+		if value != " ": continue
+		neigh.append([x+dx, y+dy])
 	return neigh
 
-visited: list[Vertex] = []
 
-
-def checkIfVisited(toCheck: Vertex):
-	for vertex in visited:
-		if vertex.x == toCheck.x and vertex.y == vertex.y: return True
-	return False
-
-def bfs(start: Vertex):
-	global visited
-	visited.append(start)
-	queue = [start]
-
+def bfs(x: int, y: int):
+	queue = [[x, y]]
+	visited = [[False for i in range(len(data))]
+				for j in range(len(data[0]))]
+	longestDist = 0
 	while queue:
 		current = queue.pop(0)
-		print(len(queue), len(visited))
-		neighbours = current.findNeighbors()
-		for neighbor in neighbours:
-			if not checkIfVisited(neighbor):
-				visited.append(neighbor)
-				queue.append(neighbor)
+		neighbors = getNeighbors(current[0], current[1])
+		data[current[0]][current[1]] = str(longestDist)
+		longestDist += 1
+		for neighbor in neighbors:
+			if visited[neighbor[0]][neighbor[1]]: continue
+			queue.append(neighbor)
+			visited[neighbor[0]][neighbor[1]] = True
+	return longestDist
+print(bfs(1, 1))
 
-bfs(Vertex(1, 1, 0))
-
-longest = 0
-for v in visited:
-	print(v.x, v.y)
-	if v.dist > longest: longest = v.dist
-print(longest)
+with open("ar2.out", "w+") as file:
+	file.write("\n".join([" ".join(["{:3s}".format(item) for item in row]) for row in data]))
